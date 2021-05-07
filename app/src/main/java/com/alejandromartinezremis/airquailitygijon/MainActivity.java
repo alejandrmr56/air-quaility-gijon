3e5af89ca9f599a5ef8dcf507983a778413b1c29
Alejandro Martínez <alejandrmr56@educastur.es>
2021-05-08 01:56:53 +0200

    Add alert dialogs to main menu items
    - Add alert dialog to 'info' menu item
    - Add alert dialog to 'about' menu item
    - Create specific style for dialogs

--- app/src/main/java/com/alejandromartinezremis/airquailitygijon/MainActivity.java
+++ app/src/main/java/com/alejandromartinezremis/airquailitygijon/MainActivity.java
@@ -1,9 +1,13 @@
 package com.alejandromartinezremis.airquailitygijon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +21,42 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                updateData();
+                break;
+            case R.id.menu_info:
+                displayInfoAlertDialog(this);
+                break;
+            case R.id.menu_about:
+                displayAboutAlertDialog(this);
+                break;
+        }
+        return true;
+    }
+
+
+    private void updateData(){
+        throw new UnsupportedOperationException("Not implemented yet"); //TODO: Implement this
+    }
+
+    private void displayInfoAlertDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Theme_AirQuailityGijon_Dialog);
+        builder.setTitle(R.string.menu_info_alert_dialog_title)
+                .setMessage(R.string.menu_info_alert_dialog_message)
+                .setPositiveButton(R.string.ok, null)
+                .create().show();
+    }
+
+    private void displayAboutAlertDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.Theme_AirQuailityGijon_Dialog);
+        builder.setTitle(R.string.menu_about_alert_dialog_title)
+                .setMessage(R.string.menu_about_alert_dialog_message)
+                .setPositiveButton(R.string.ok, null)
+                .create().show();
     }
 }
