eec35518ba094f8528bef1068c4df879cacbdad1
Alejandro Martínez <alejandrmr56@educastur.es>
2021-05-19 01:54:00 +0200

    Add login activity (GUI only)
    - Create login activity and make it the main one
    - Add light grey colour
    - Add main app icon as a regular picture
    - Update the colour palette for EditText

---
+++ app/src/main/java/com/alejandromartinezremis/airquailitygijon/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.alejandromartinezremis.airquailitygijon.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alejandromartinezremis.airquailitygijon.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
